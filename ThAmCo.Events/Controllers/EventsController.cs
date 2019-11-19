ab4cfc618d73f8b95064305d0a043778c80b99ba
BEN RAMSDEN <V8036651@live.tees.ac.uk>
2019-11-19 14:34:28 +0000

    Query the table that is required for reservations.

--- ThAmCo.Events/Controllers/EventsController.cs
+++ ThAmCo.Events/Controllers/EventsController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -159,6 +161,93 @@ namespace ThAmCo.Events.Controllers
         private bool EventExists(int id)
         {
             return _context.Events.Any(e => e.Id == id);
+        }
+
+        // GET: Events/Reservations/5
+        public async Task<IActionResult> Reservations(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Events
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            HttpClient client = new HttpClient();
+            var Builder = new UriBuilder("http://localhost");
+            Builder.Port = 23652;
+            var query = HttpUtility.ParseQueryString(Builder.Query);
+            query["eventType"] = @event.TypeId;
+            query["eventDate"] = @event.Date.ToString("yyyy/MM/dd HH:mm:ss");
+            query["eventDate"] = @event.Date.Add(@event.Duration.Value).ToString();
+
+            Builder.Query = query.ToString();
+            string url = Builder.ToString();
+
+            client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
+
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                ViewData["Reservations"] = response;
+
+                return View();
+            }
+
+            return View(@event);
+            //return RedirectToAction(nameof(Index));
+        }
+
+
+
+        // POST: Events/Reservations/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Reservations(int id, string Title, TimeSpan Duration, Event @event)
+        {
+            if (id != @event.Id)
+            {
+                return NotFound();
+            }
+
+            if (string.IsNullOrEmpty(Title) || Duration == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                try
+                {
+                    Event e = await _context.Events.FindAsync(id);
+                    e.Title = Title;
+                    e.Duration = Duration;
+                    await _context.SaveChangesAsync();
+                    _context.Events.Update(e);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EventExists(@event.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@event);
+            //Must save to reservation table, requires: Staff Id, Date made, Event date, Venue code, Avalibility, Reference
+            //Must load from find where event type == suitable event type
         }
     }
 }
