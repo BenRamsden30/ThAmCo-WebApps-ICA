c4b55e2ad51aecc04197a792d14ff6a16c6e3a13
BEN RAMSDEN <V8036651@live.tees.ac.uk>
2019-12-03 14:59:31 +0000

    Ermmmmmmmmmmm errors? partial view working on

--- ThAmCo.Events/Controllers/EventsController.cs
+++ ThAmCo.Events/Controllers/EventsController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ThAmCo.Events.Data;
-using ThAmCo.Venues.Data;
+
 
 namespace ThAmCo.Events.Controllers
 {
@@ -164,126 +164,6 @@ namespace ThAmCo.Events.Controllers
         private bool EventExists(int id)
         {
             return _context.Events.Any(e => e.Id == id);
-        }
-
-        // GET: Events/Reservations/5
-        public async Task<IActionResult> Reservations(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var @event = await _context.Events
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (@event == null)
-            {
-                return NotFound();
-            }
-
-            HttpClient client = new HttpClient();
-            var Builder = new UriBuilder("http://localhost");
-            Builder.Port = 23652;
-            Builder.Path = "api/Availability";
-            var query = HttpUtility.ParseQueryString(Builder.Query);
-            query["eventType"] = @event.TypeId;
-            query["beginDate"] = @event.Date.ToString("yyyy/MM/dd HH:mm:ss");
-            query["endDate"] = @event.Date.Add(@event.Duration.Value).ToString("yyyy/MM/dd HH:mm:ss");
-
-            Builder.Query = query.ToString();
-            string url = Builder.ToString();
-
-            client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
-
-            HttpResponseMessage response = await client.GetAsync(url);
-
-            if (response.IsSuccessStatusCode)
-            {
-                var venue = await response.Content.ReadAsAsync<IEnumerable<Venue>>();
-                if(venue.Count() == 0)
-                {
-                    ModelState.AddModelError("", "No Suitable venues could be found.");
-                }
-
-                ViewData["venues"] = new SelectList(venue, "Code", "Name");
-                
-                
-                
-                
-
-                return View(@event);
-            }
-
-
-            if (response.IsSuccessStatusCode)
-            {
-                ViewData["Reservations"] = response;
-
-                return View(@event);
-            }
-
-            return View(@event);
-            //return RedirectToAction(nameof(Index));
-        }
-
-
-
-        // POST: Events/Reservations/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Reservations(int id, string TypeId, TimeSpan Duration, Event @event, DateTime date)
-        {
-            if (id != @event.Id)
-            {
-                return NotFound();
-            }
-
-            if (string.IsNullOrEmpty(TypeId) || Duration == null)
-            {
-                return NotFound();
-            }
-            else
-            {
-                try
-                {
-                    var Builder = new UriBuilder("http://localhost");
-                    Builder.Port = 23652;
-                    Builder.Path = "api/Availability";
-
-                    var query = HttpUtility.ParseQueryString(Builder.Query);
-                    query["eventType"] = TypeId;
-                    query["beginDate"] = date.ToString("yyyy/MM/dd HH:mm:ss");
-                    query["endDate"] = date.Add(Duration).ToString("yyyy/MM/dd HH:mm:ss");
-
-                    Builder.Query = query.ToString();
-                    string url = Builder.ToString();
-
-                    // put the update in here
-
-                    await _context.SaveChangesAsync();
-
-                    HttpClient client = new HttpClient();
-                    client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
-                    HttpResponseMessage response = await client.GetAsync(url);
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!EventExists(@event.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(@event);
-            //Must save to reservation table, requires: Staff Id, Date made, Event date, Venue code, Avalibility, Reference
-            //Must load from find where event type == suitable event type
         }
     }
 }
---
+++ ThAmCo.Events/Controllers/ReservationsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ThAmCo.Events.Data;
+
+namespace ThAmCo.Events.Controllers
+{
+
+    public class ReservationsController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:23652");
+            client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
+            client.Timeout = TimeSpan.FromSeconds(5);
+
+            var response = await client.GetAsync("/api/reservations/");
+            response.EnsureSuccessStatusCode();
+            IEnumerable<Reservation> venue = await response.Content.ReadAsAsync<IEnumerable<Reservation>>();
+
+            return View(venue);
+        }
+    }
+}
