be21b21a83186b1fddf64f1e134024c10022ba2a
BEN RAMSDEN <V8036651@live.tees.ac.uk>
2020-01-04 18:13:47 +0000

    Migrated database ad updated it

---
+++ ThAmCo.Events/Data/Migrations/20200104181221_StaffTablesAdded.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ThAmCo.Events.Data.Migrations
+{
+    public partial class StaffTablesAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StaffBookings",
+                schema: "thamco.events",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    StaffId = table.Column<int>(nullable: false),
+                    EventId = table.Column<int>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StaffBookings", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_StaffBookings_Events_EventId",
+                        column: x => x.EventId,
+                        principalSchema: "thamco.events",
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StaffBookings_EventId",
+                schema: "thamco.events",
+                table: "StaffBookings",
+                column: "EventId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StaffBookings",
+                schema: "thamco.events");
+        }
+    }
+}
--- ThAmCo.Events/Data/StaffBookings.cs
+++ ThAmCo.Events/Data/StaffBookings.cs
@@ -6,6 +6,8 @@ namespace ThAmCo.Events.Data
 {
     public class StaffBookings
     {
+        public int id { get; set; }
+
         public int StaffId { get; set; }
 
         public int EventId { get; set; }
